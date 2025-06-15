4b07cc60790ae9a7f13a5afc4ada678d1ef60ab3
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-06-15 00:16:06 -0300

    agregue maquetado a ListaTareas

---
+++ src/components/ListaTareas.jsx
@@ -0,0 +1,14 @@
+import ListGroup from "react-bootstrap/ListGroup";
+import ItemTarea from "./ItemTarea"
+
+const ListaTareas = () => {
+  return (
+    <>
+      <ListGroup> 
+          <ItemTarea/>
+      </ListGroup>
+    </>
+  );
+};
+
+export default ListaTareas;
